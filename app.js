b1e1ca4b14a7ff7bd50ceb10462eb2f7bb7a3a11
Shmulik <shmulikmak@gmail.com>
2018-01-17 00:39:57 +0200

    add cors and Error

--- app.js
+++ app.js
@@ -7,8 +7,21 @@ const productRouts = require('./api/routes/products');
 const orderRouts = require('./api/routes/order');
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use('products', productRouts);
 app.use('order', orderRouts);
